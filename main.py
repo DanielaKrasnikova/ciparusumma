53b43735e4edb6b7dc8d4d287535a4e842a99dbc
DanaKrasnikova <77327843+DanielaKrasnikova@users.noreply.github.com>
2022-01-18 12:59:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+teksts = input("ievadit tekstu:")
+def countnumbers(teksts):
+  summa = 0
+  for simbols in teksts :
+    summa = summa = int(simbols)
+  print (summa)
+  return summa
+countnumbers(teksts)
